e8a821b59ab69c9c0cbd49dbed88ec1fbf439f32
N-Mur35 <56538253+N-Mur35@users.noreply.github.com>
2020-03-12 19:46:48 +0000

    changes 3

--- peptide.py
+++ peptide.py
@@ -8,7 +8,7 @@ def peptideBasics(sequence, Nterm, Cterm):
 
   #resp = requests.get("http://api.pep-calc.com/" + sequence + "&N_term=" + Nterm + "&C_term=" + Cterm + "&mz=")
 
-  resp2 = requests.get("http://api.pep-calc.com/peptide/betacontiguity?seq=" + sequence + "&N_term=" + Nterm + "&C_term=" + Cterm + "&mz=")
+  #resp2 = requests.get("http://api.pep-calc.com/peptide/betacontiguity?seq=" + sequence + "&N_term=" + Nterm + "&C_term=" + Cterm + "&mz=")
 
   print(resp)
   data = resp.json()
