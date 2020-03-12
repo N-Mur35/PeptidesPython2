2769630dd139bb1c3bd3171409635fc142518770
N-Mur35 <56538253+N-Mur35@users.noreply.github.com>
2020-03-12 19:26:34 +0000

    changw

--- peptide.py
+++ peptide.py
@@ -8,9 +8,12 @@ def peptideBasics(sequence, Nterm, Cterm):
 
   #resp = requests.get("http://api.pep-calc.com/" + sequence + "&N_term=" + Nterm + "&C_term=" + Cterm + "&mz=")
 
+  resp2 = requests.get("http://api.pep-calc.com/peptide/betacontiguity?seq=" + sequence + "&N_term=" + Nterm + "&C_term=" + Cterm + "&mz=")
+
   print(resp)
   data = resp.json()
   return data
+
 
 data = peptideBasics("PSICHVHRPDWPCWYR", 'H', 'OH')
 print(data)
